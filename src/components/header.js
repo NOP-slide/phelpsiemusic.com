ea64140f950ed9ec9119e8d16a8ad289e8c2923a
Andrew Phelps <59522519+NOP-slide@users.noreply.github.com>
2023-10-22 02:15:07 +0800

    add cart item counter

--- src/components/header.js
+++ src/components/header.js
@@ -5,33 +5,34 @@ import { HiOutlineShoppingCart } from "react-icons/hi"
 import { useSiteContext } from "../hooks/use-site-context"
 
 const Header = () => {
-  const { setIsCartOpen } = useSiteContext()
+  const { setIsCartOpen, cartItemsFromLS } = useSiteContext()
 
   return (
     <header>
       <div className={`w-full bg-brand-dark py-4 px-6 lg:px-6`}>
         <div className="relative">
-          <div className="flex items-center justify-between w-full">
+          <div className="relative flex items-center justify-between w-full">
             <Link to="/">
               <StaticImage
                 quality={95}
                 src="../images/logo.png"
                 placeholder="none"
                 alt=""
-                className="w-52 sm:w-60"
+                className="flex items-center w-48 sm:w-60"
               />
             </Link>
-            <HiOutlineShoppingCart
+            <div
               onClick={() => setIsCartOpen(true)}
-              className="text-3xl cursor-pointer sm:text-4xl text-brand-teal"
-            />
+              className="flex cursor-pointer"
+            >
+              {cartItemsFromLS.length > 0 && (
+                <p className="absolute flex items-center px-3 font-bold text-gray-200 bg-red-600 rounded-full top-2 sm:top-1.5 sm:right-9 right-7 text-xs sm:text-base">
+                  {JSON.parse(cartItemsFromLS).items.length}
+                </p>
+              )}
+              <HiOutlineShoppingCart className="text-3xl sm:text-4xl text-brand-teal" />
+            </div>
           </div>
-          {/* <div className="absolute hidden italic font-semibold tracking-wider text-gray-300 -translate-y-1/2 whitespace-nowrap sm:text-sm sm:pl-10 md:pl-0 sm:-translate-x-1/4 lg:-translate-x-1/3 md:text-lg lg:text-xl sm:block top-1/2 left-1/2">
-            Premium Tools for the Discerning Producer
-          </div>
-          <div className="block mt-6 text-sm italic font-semibold tracking-wider text-center text-gray-300 whitespace-nowrap sm:hidden">
-            Premium Tools for the Discerning Producer
-          </div> */}
         </div>
       </div>
     </header>
