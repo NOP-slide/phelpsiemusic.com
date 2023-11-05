8803208d43f07d4ae41cd6749d75beff67614790
Andrew Phelps <59522519+NOP-slide@users.noreply.github.com>
2023-11-05 14:19:12 +0800

    add abandoned cart auto email

--- functions/stripe-event-handler.js
+++ functions/stripe-event-handler.js
@@ -87,31 +87,28 @@ exports.handler = async ({ body, headers }) => {
 
       if (email && recoveryUrl) {
         try {
-          const res = await fetch(
-            "https://connect.mailerlite.com/api/subscribers",
-            {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-                Authorization:
-                  "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiNTZkN2ViYmNhOTNiYTYwYWU1ZjhlY2MwZDkxN2JmYzE3MWExM2FjY2FmYTI0OGUzOTRjYTMxNzFkYzJlOGY2Mzk2ODYwNTdmY2EwODJjZjIiLCJpYXQiOjE2OTczMDI2MzEuNDQzOTksIm5iZiI6MTY5NzMwMjYzMS40NDM5OTIsImV4cCI6NDg1Mjk3NjIzMS40Mzk4NTYsInN1YiI6IjU5NTA0MCIsInNjb3BlcyI6W119.tK7vkOIUzMLX1zfxmJA6SofJfRetwAc4k6P-6qQ_hyhErCLBAZC-oq3kpyZHsgLopoqIkQSKOAJ_HsHGjgb8Ar1NQiiGcCoql3QCXAYzAJCC8qiOW-OVHf3KoS5gWbbD-R6juOvsP32Vm-PA2f54eqOrHOIRoh0bX7aO4ZSxK1bl6L07u3avVgSduyWzkgXQzMRbZ5M0a8POn5JCnvrMVrxu-w0L48hRT5daossj4HhdaUsA72VtBH_EFoPiQEDIOxdbThtqsMNLxmePsC6nz2o__41dqheKfAhbBurhUpgtovO0QQNoUCOE-PJwZLjM6KLqmNCtfRIrzEL_HwDUK8Vvnp_And8bvUvf4TBIwptXiJw-6v4cvBrkpn37Zc6s0J8wk5qqNYhgQVButjR9Wj2oDHJ1pOF9IASslsqLrowwwQjoAN4d699gGUrb9aofv1o7yKTN3S-Ed-sOMUuMM_ybWKgxzkcCPY0rcVHLTo8T52dBjHD5L4JQOdqnaD96ehNds0uK6pZ1Q0BsGYba5Ucpopd4T5_DfuAh1bmEFjmD8z-BBag382jp0_eOTkH1wsT4NstuFJzbTvI-Jn1G5FyaGkc3-0DuWuOhjnpTImjx7lHxhzmt7lausraIqO3cve3O6XhQtBNdJqOyHeVBIU29leB-whazwWOA_yfdNCs",
-                Accept: "application/json",
-              },
-              body: JSON.stringify({
-                email: email,
-                fields: {
-                  name: session.customer_details.name
-                    ? session.customer_details.name
-                    : "Unknown Name",
-                  last_name: "",
-                  recovery_url: recoveryUrl,
-                },
-                groups: ["102709796762813970"],
-              }),
-            }
+          let emailHtml = `<p>Hey bro,</p><p>Thanks for checking out the loops on my website, I appreciate it. I noticed you didn't complete your purchase - no worries if you don't feel like these loops are right for your music, but if you change your mind, use this link and enter the coupon code WELCOMEBACK to receive 15% off your order.</p>`
+
+          emailHtml +=
+            `<a href="` + recoveryUrl + `">Complete your order here</a><br/>`
+
+          emailHtml += `<br/><p>Your friend,</p><p>Phelpsie</p>`
+
+          const sentFrom = new Sender(
+            "phelpsie@phelpsiemusic.com",
+            "Phelpsie Music"
           )
-          const data = await res.json()
-          console.log("Return from mailerlite =", data)
+          const recipients = [new Recipient(email, "")]
+
+          const emailParams = new EmailParams()
+            .setFrom(sentFrom)
+            .setTo(recipients)
+            .setReplyTo(sentFrom)
+            .setSubject(
+              `Did you forget something? Get 15% off right now at Phelpsie Music`
+            )
+            .setHtml(emailHtml)
+          const res = await mailerSend.email.send(emailParams)
         } catch (error) {
           // eslint-disable-next-line no-console
           console.error(error)
