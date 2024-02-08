c39e1a5b9e6e804fe59c8611d04e6949c2971184
Andrew Phelps <59522519+NOP-slide@users.noreply.github.com>
2024-02-08 13:27:41 +0800

    switch midi crate abandoned cart emails to mailerlite

--- functions/stripe-event-handler.js
+++ functions/stripe-event-handler.js
@@ -205,30 +205,33 @@ exports.handler = async ({ body, headers }) => {
           console.error(error)
         }
       }
+
+      // ********************************
+      // MIDI Crate Abandoned Cart
+      // ********************************
       if (email && recoveryUrl && session.mode === "subscription") {
         try {
-          let emailHtml = `<p>Hey producer,</p><p>Thanks for checking out my website. I appreciate you! I noticed that you didn't finish downloading your free month of MIDI Crate, so I wanted to personally reach out and let you know that the offer is still on the table if you're interested: 360 mind-blowing MIDI files - including chord progressions and arpeggios - to save you tons of time and provide the perfect spark of inspiration for your next track. If you'd like to access your free trial month now, you can do so by clicking this link: </p>`
-
-          emailHtml +=
-            `<a href="` + recoveryUrl + `">Get My Free MIDI Crate Now</a><br/>`
-
-          emailHtml += `<p>I am dedicated to providing only the highest quality musical tools, and building a community of like-minded music producers to share knowledge and grow together. I'd love for you to join us, so if you have any questions or concerns on your mind, please feel free to let me know and I'm sure I can help you out.</p><br/><p>Your friend in music,</p><p>Phelpsie</p>`
-
-          const sentFrom = new Sender(
-            "phelpsie@phelpsiemusic.com",
-            "Phelpsie Music"
+          const res = await fetch(
+            "https://connect.mailerlite.com/api/subscribers",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization:
+                  "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiNTZkN2ViYmNhOTNiYTYwYWU1ZjhlY2MwZDkxN2JmYzE3MWExM2FjY2FmYTI0OGUzOTRjYTMxNzFkYzJlOGY2Mzk2ODYwNTdmY2EwODJjZjIiLCJpYXQiOjE2OTczMDI2MzEuNDQzOTksIm5iZiI6MTY5NzMwMjYzMS40NDM5OTIsImV4cCI6NDg1Mjk3NjIzMS40Mzk4NTYsInN1YiI6IjU5NTA0MCIsInNjb3BlcyI6W119.tK7vkOIUzMLX1zfxmJA6SofJfRetwAc4k6P-6qQ_hyhErCLBAZC-oq3kpyZHsgLopoqIkQSKOAJ_HsHGjgb8Ar1NQiiGcCoql3QCXAYzAJCC8qiOW-OVHf3KoS5gWbbD-R6juOvsP32Vm-PA2f54eqOrHOIRoh0bX7aO4ZSxK1bl6L07u3avVgSduyWzkgXQzMRbZ5M0a8POn5JCnvrMVrxu-w0L48hRT5daossj4HhdaUsA72VtBH_EFoPiQEDIOxdbThtqsMNLxmePsC6nz2o__41dqheKfAhbBurhUpgtovO0QQNoUCOE-PJwZLjM6KLqmNCtfRIrzEL_HwDUK8Vvnp_And8bvUvf4TBIwptXiJw-6v4cvBrkpn37Zc6s0J8wk5qqNYhgQVButjR9Wj2oDHJ1pOF9IASslsqLrowwwQjoAN4d699gGUrb9aofv1o7yKTN3S-Ed-sOMUuMM_ybWKgxzkcCPY0rcVHLTo8T52dBjHD5L4JQOdqnaD96ehNds0uK6pZ1Q0BsGYba5Ucpopd4T5_DfuAh1bmEFjmD8z-BBag382jp0_eOTkH1wsT4NstuFJzbTvI-Jn1G5FyaGkc3-0DuWuOhjnpTImjx7lHxhzmt7lausraIqO3cve3O6XhQtBNdJqOyHeVBIU29leB-whazwWOA_yfdNCs",
+                Accept: "application/json",
+              },
+              body: JSON.stringify({
+                email: email,
+                fields: {
+                  recovery_url: recoveryUrl,
+                },
+                groups: ["112583430793856778"],
+              }),
+            }
           )
-          const recipients = [new Recipient(email, "")]
-
-          const emailParams = new EmailParams()
-            .setFrom(sentFrom)
-            .setTo(recipients)
-            .setReplyTo(sentFrom)
-            .setSubject(
-              `Your MIDI Crate is waiting: 360 Free MIDI files + bonuses`
-            )
-            .setHtml(emailHtml)
-          const res = await mailerSend.email.send(emailParams)
+          const data = await res.json()
+          console.log("Return from mailerlite =", data)
         } catch (error) {
           // eslint-disable-next-line no-console
           console.error(error)
