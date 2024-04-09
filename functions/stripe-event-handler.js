72d630d1ba1ea9071614d1e6553fedffa8af2319
Andrew Phelps <59522519+NOP-slide@users.noreply.github.com>
2024-04-09 22:22:59 +0800

    add month 3 event code

--- functions/stripe-event-handler.js
+++ functions/stripe-event-handler.js
@@ -221,7 +221,36 @@ exports.handler = async ({ body, headers }) => {
       if (stripeEvent.data.object.amount_paid > 0) {
         let customerEmail = stripeEvent.data.object.customer_email
 
+        let customerData
+        // Get subscriber ID from Mailerlite
+        try {
+          const res = await fetch(
+            `https://connect.mailerlite.com/api/subscribers/${customerEmail}`,
+            {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization:
+                  "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiNTZkN2ViYmNhOTNiYTYwYWU1ZjhlY2MwZDkxN2JmYzE3MWExM2FjY2FmYTI0OGUzOTRjYTMxNzFkYzJlOGY2Mzk2ODYwNTdmY2EwODJjZjIiLCJpYXQiOjE2OTczMDI2MzEuNDQzOTksIm5iZiI6MTY5NzMwMjYzMS40NDM5OTIsImV4cCI6NDg1Mjk3NjIzMS40Mzk4NTYsInN1YiI6IjU5NTA0MCIsInNjb3BlcyI6W119.tK7vkOIUzMLX1zfxmJA6SofJfRetwAc4k6P-6qQ_hyhErCLBAZC-oq3kpyZHsgLopoqIkQSKOAJ_HsHGjgb8Ar1NQiiGcCoql3QCXAYzAJCC8qiOW-OVHf3KoS5gWbbD-R6juOvsP32Vm-PA2f54eqOrHOIRoh0bX7aO4ZSxK1bl6L07u3avVgSduyWzkgXQzMRbZ5M0a8POn5JCnvrMVrxu-w0L48hRT5daossj4HhdaUsA72VtBH_EFoPiQEDIOxdbThtqsMNLxmePsC6nz2o__41dqheKfAhbBurhUpgtovO0QQNoUCOE-PJwZLjM6KLqmNCtfRIrzEL_HwDUK8Vvnp_And8bvUvf4TBIwptXiJw-6v4cvBrkpn37Zc6s0J8wk5qqNYhgQVButjR9Wj2oDHJ1pOF9IASslsqLrowwwQjoAN4d699gGUrb9aofv1o7yKTN3S-Ed-sOMUuMM_ybWKgxzkcCPY0rcVHLTo8T52dBjHD5L4JQOdqnaD96ehNds0uK6pZ1Q0BsGYba5Ucpopd4T5_DfuAh1bmEFjmD8z-BBag382jp0_eOTkH1wsT4NstuFJzbTvI-Jn1G5FyaGkc3-0DuWuOhjnpTImjx7lHxhzmt7lausraIqO3cve3O6XhQtBNdJqOyHeVBIU29leB-whazwWOA_yfdNCs",
+                Accept: "application/json",
+              },
+            }
+          )
+          customerData = await res.json()
+          console.log("Return from mailerlite =", customerData)
+        } catch (error) {
+          // eslint-disable-next-line no-console
+          console.error(error)
+        }
+
         // Add to month x of MIDI Crate
+        let month = 2;
+        let group = ["114573246580393257"];
+
+        if (customerData.data.fields.midi_crate_month === 2) {
+          month = 3;
+          group = ["118132620505646144"];
+        }
         try {
           const res = await fetch(
             "https://connect.mailerlite.com/api/subscribers",
@@ -236,9 +265,9 @@ exports.handler = async ({ body, headers }) => {
               body: JSON.stringify({
                 email: customerEmail,
                 fields: {
-                  midi_crate_month: 2,
+                  midi_crate_month: month,
                 },
-                groups: ["114573246580393257"],
+                groups: group,
               }),
             }
           )
